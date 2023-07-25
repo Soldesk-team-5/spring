e339719ce371554141ceb2c0e70844e4e035da40
이름 <galeb12243@gmail.com>
2023-07-26 08:10:19 +0900

    update
    update

--- src/main/java/com/soldesk/TeamProject/TeamProjectApplication.java
+++ src/main/java/com/soldesk/TeamProject/TeamProjectApplication.java
@@ -2,12 +2,17 @@ package com.soldesk.TeamProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeamProjectApplication {
-
+public class TeamProjectApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(TeamProjectApplication.class);
+	};
 	public static void main(String[] args) {
 		SpringApplication.run(TeamProjectApplication.class, args);
-	}
+	};
 
-}
+};
