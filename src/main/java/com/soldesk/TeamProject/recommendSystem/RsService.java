86e9d91981aee777dd62a3ad0ee11ae739c8fe72
이름 <galeb12243@gmail.com>
2023-08-02 15:03:32 +0900

    change path
    change path

--- src/main/java/com/soldesk/TeamProject/recommendSystem/RsService.java
+++ src/main/java/com/soldesk/TeamProject/recommendSystem/RsService.java
@@ -28,7 +28,7 @@ public class RsService {
     @PostConstruct
     public void loadWord2VecModel() throws Exception{
         System.setProperty("java.library.path", "/path/to/nd4j/library");
-        File file = new File("/src/main/resources/static/model/word2vec.bin.gz");
+        File file = new File("src/main/resources/static/model/word2vec.bin.gz");
 
         try {
             System.out.println(file);
