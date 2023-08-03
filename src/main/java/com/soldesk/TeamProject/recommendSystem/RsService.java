9aebfb5d1ed5f40ddcb952402280fef50abb347f
이름 <galeb12243@gmail.com>
2023-08-04 07:03:28 +0900

    update
    update

--- src/main/java/com/soldesk/TeamProject/recommendSystem/RsService.java
+++ src/main/java/com/soldesk/TeamProject/recommendSystem/RsService.java
@@ -28,8 +28,8 @@ public class RsService {
     @PostConstruct
     public void loadWord2VecModel() throws Exception{
         System.setProperty("java.library.path", "/path/to/nd4j/library");
-        File file = new File("src/main/resources/static/model/word2vec.bin.gz");
-        //File file = new File("word2vec.bin.gz");
+        //File file = new File("src/main/resources/static/model/word2vec.bin.gz");
+        File file = new File("word2vec.bin.gz");
 
         try {
             this.model = WordVectorSerializer.readWord2VecModel (file);
